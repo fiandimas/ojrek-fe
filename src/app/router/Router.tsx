32cfe638d2424ccb29e6d58821cdc2d6d28d4fef
fiandimas <alfndimas@gmail.com>
2025-12-26 15:32:20 +0700

    feat: job page

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router";
 
+import HomePage from '@/features/home/HomePage';
+import JobPage from '@/features/job/JobPage';
 import RegisterPage from '@/features/auth/register/RegisterPage';
 
 
 const Router = [
+  {
+    path: '/',
+    element: <HomePage/>
+  },
+  {
+    path: '/job',
+    element: <JobPage/>
+  },
   // Guest routes - authentication pages
   {
     path: '/auth',
---
+++ src/features/job/JobPage.tsx
@@ -0,0 +1,74 @@
+import TopBar from "@/shared/components/TopBar";
+import { AppBar, Box, Button, Card, CardActionArea, CardActions, CardContent, Container, Grid, Menu, TextField, Toolbar, Typography } from "@mui/material";
+import InputAdornment from '@mui/material/InputAdornment';
+import { useEffect, useState } from "react";
+
+const JobPage: React.FC = () => {
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://127.0.0.1:8080/api/v1/jobs?limit=100');
+        const json = await res.json();
+        setData(json.data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <Box>
+      <TopBar />
+      <Container maxWidth="xl">
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: 2, marginTop: 2 }}>
+          <TextField
+            placeholder="Search by title..."
+            size="small"
+            fullWidth
+          />
+          <Button variant="contained" size="small" sx={{ width: 200 }}>Search</Button>
+        </Box>
+
+
+
+        <Box sx={{ display: 'flex', marginTop: 4 }}>
+          <Box sx={{ background: 'red'}}>
+            <Typography variant="h6">Jobs in Indonesia</Typography>
+          </Box>
+          <Box sx={{ width: '100%' }}>
+            <Grid container spacing={2}>
+              {data.map((e) => (
+                <Grid size={4}>
+                  <Card>
+                    <CardActionArea onClick={() => open(e.detail_url)}>
+                      <CardContent>
+                      <Typography gutterBottom variant="h6">
+                        {e.name}
+                      </Typography>
+                    </CardContent>
+                    <CardActions sx={{ display: 'flex', justifyContent: 'space-between'}}>
+                      <Typography variant="h6" fontSize={13}>2 days ago</Typography>
+                      <Button size="small">Share</Button>
+                    </CardActions>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+}
+
+export default JobPage;
