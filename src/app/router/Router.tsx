73a8fec6b0a0e6164d3262ebc265cce7a50714c3
fiandimas <alfndimas@gmail.com>
2025-12-27 04:22:27 +0700

    fix: route constants

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -3,24 +3,32 @@ import { createBrowserRouter } from "react-router";
 import HomePage from '@/features/home/HomePage';
 import JobPage from '@/features/job/JobPage';
 import RegisterPage from '@/features/auth/register/RegisterPage';
-
+import LoginPage from '@/features/auth/login/LoginPage';
+import AppLayout from "@/shared/layouts/AppLayout";
+import { ROUTES } from "@/constants/router";
 
 const Router = [
   {
-    path: '/',
-    element: <HomePage/>
-  },
-  {
-    path: '/job',
-    element: <JobPage/>
-  },
-  // Guest routes - authentication pages
-  {
-    path: '/auth',
+    path: ROUTES.ROOT,
+    element: <AppLayout />,
     children: [
-      { path: 'login', element: <RegisterPage /> },
+      {
+        path: '',
+        element: <HomePage/>
+      },
+      {
+        path: ROUTES.JOBS,
+        element: <JobPage/>
+      },
+      // Guest routes - authentication pages
+      {
+        path: ROUTES.AUTH.LOGIN,
+        element: <LoginPage />,
+      },
+      { path: ROUTES.AUTH.REGISTER, element: <RegisterPage /> },
     ],
   },
+  
 ];
 
 const router = createBrowserRouter(Router)
---
+++ src/constants/router.ts
@@ -0,0 +1,8 @@
+export const ROUTES = {
+  ROOT: '/',
+  JOBS: '/jobs',
+  AUTH: {
+    LOGIN: '/auth/login',
+    REGISTER: '/auth/register'
+  }
+}
